614999f8e7aa42569faf961d56f608b41f75cc40
nanofercho91 <nanoreturn@gmail.com>
2021-01-12 22:13:49 -0500

    cambios realizados

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editar <code>src/App.js</code> and save to reload.
+          Editar este archivo para ver como cambia <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
